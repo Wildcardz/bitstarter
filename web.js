893aa62eb0493790f55427b4f295456d50f8afbe
Wildcardz <lo.norman@gmail.com>
2013-07-10 04:47:42 +0000

    add toString

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    
-  response.send('Hello World 2!');
+  response.send(buf.ToString());
 });
 
 var port = process.env.PORT || 5000;
