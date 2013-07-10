35cd8bfd733e3d35cdbcd86042c94321325c77db
Wildcardz <lo.norman@gmail.com>
2013-07-10 09:38:33 +0000

    Version 1 of rester integration

--- grader.js
+++ grader.js
@@ -21,11 +21,15 @@ References:
    - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
+var util = require('util');
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://arcane-brushlands-4998.herokupp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -55,6 +59,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url) {
+    rest.get(url).on('complete', function(result) {
+        if (result instanceof Error) {
+	     util.puts('Error: ' + result.message);
+	     this.retry(5000);		// try again in 5 sec
+	} else {
+	     util.puts(result);
+	}
+    });
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,6 +80,7 @@ if(require.main == module) {
     program
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to url', clone(checkUrl), URL_DEFAULT)
         .parse(process.argv);
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
