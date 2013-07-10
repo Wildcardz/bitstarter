bd96ac8b7c8232ec0ffd45eee1212b85e0ad1890
Wildcardz <lo.norman@gmail.com>
2013-07-10 04:49:03 +0000

    change case of toString

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
    
-  response.send(buf.ToString());
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
